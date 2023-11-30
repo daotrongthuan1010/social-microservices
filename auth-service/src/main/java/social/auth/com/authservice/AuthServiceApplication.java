91b8365524915d845c8805b8c0eb8445acbcdd0e
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-11-30 23:15:38 +0700

    [BE][Auth-service]: Add service for service registry

--- auth-service/src/main/java/social/auth/com/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/social/auth/com/authservice/AuthServiceApplication.java
@@ -2,9 +2,10 @@ package social.auth.com.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
 
